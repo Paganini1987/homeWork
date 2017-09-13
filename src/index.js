39482b90d798312a3d98f15204dff4b7411e5c0b
Pavel Ivonin <pavelivonin@yandex.ru>
2017-09-13 20:48:43 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,80 +1,25 @@
-/* ДЗ 1 - Функции */
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
 
-/*
- Задание 1:
-
- Функция должна принимать один аргумент и возвращать его
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
  */
-function returnFirstArgument(arg) {
-	return arg;
+function delayPromise(seconds) {
 }
 
-/*
- Задание 2:
-
- Функция должна принимать два аргумента и возвращать сумму переданных значений
- Значение по умолчанию второго аргумента должно быть 100
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
  */
-function defaultParameterValue(a, b) {
-	return a + (b || 100);
-}
-
-/*
- Задание 3:
-
- Функция должна возвращать все переданные в нее аргументы в виде массива
- Количество переданных аргументов заранее неизвестно
- */
-function returnArgumentsArray() {
-	var arr = [];
-	for(var i = 0;i<arguments.length;i++){
-		arr.push(arguments[i]);
-	}
-	return arr;
-}
-
-/*
- Задание 4:
-
- Функция должна принимать другую функцию и возвращать результат вызова переданной функции
- */
-function returnFnResult(fn) {
-	return fn();
-}
-
-/*
- Задание 5:
-
- Функция должна принимать число (значение по умолчанию - 0) и возвращать функцию (F)
- При вызове F, переданное число должно быть увеличено на единицу и возвращено из F
- */
-function returnCounter(number) {
-	var n = number || 0;
-	return function(){
-		return ++n;
-	}
-}
-
-/*
- Задание 6 *:
-
- Функция должна принимать другую функцию (F) и некоторое количество дополнительных аргументов
- Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
- */
-function bindFunction(fn) {
-  var func=arguments[0];
-  var arg=''; 				
-  for(var i=1;i<arguments.length;i++){
-    arg+=arguments[i];
-  }
-  return func.bind('null',arg);
+function loadAndSortTowns() {
 }
 
 export {
-    returnFirstArgument,
-    defaultParameterValue,
-    returnArgumentsArray,
-    returnFnResult,
-    returnCounter,
-    bindFunction
-}
+    delayPromise,
+    loadAndSortTowns
+};

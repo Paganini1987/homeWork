49f5ee027d384b51397fd3a610f40f6b8b809036
Pavel Ivonin <pavelivonin@yandex.ru>
2017-09-24 15:44:57 +0200

    add DnD

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.sass';
-var templateElement = require('./list.hbs');
-var results=document.querySelector('#list');
+var templateElement = require('./list.hbs'),
+    results=document.querySelector('#list'),
+    leftColumnArr=[],
+    rightColumnArr=[],
+    leftColumn=document.querySelector('#list'),
+    rightColumn=document.querySelector('#list2');
+    
 
 function api(method, params) {
     return new Promise(function(resolve, reject) {
@@ -28,6 +33,85 @@ var promise=new Promise(function(resolve, reject) {
     });
 }, 16);
 
+function addItem(item, where) {
+    where.appendChild(item);
+    if (where===rightColumn) {
+        addXButton(item);
+    } else {
+        addPlusButton(item);
+    }
+}
+
+function removeItem(item, where) {
+    where.removeChild(item);
+}
+
+function addXButton(item) {
+    for (var i=0;i<item.children.length;i++) {
+        if (item.children[i].id==='event_button') {
+            item.children[i].classList.remove('fa-plus');
+            item.children[i].classList.add('fa-times');
+        }
+    }
+}
+
+function addPlusButton(item) {
+    for (var i=0;i<item.children.length;i++) {
+        if (item.children[i].id==='event_button') {
+            item.children[i].classList.remove('fa-times');
+            item.children[i].classList.add('fa-plus');
+        }
+    }
+}
+
+function moveItem(...arg) {
+    var e=arg[1];
+    var listItem=arg[0];
+
+    listItem.style.left = e.pageX - listItem.offsetWidth / 2 + 'px';
+    listItem.style.top = e.pageY - listItem.offsetHeight / 2 + 'px';
+}
+
+function addListeners() {
+    leftColumn.addEventListener('click', function(e) {
+        if (e.target.id==='event_button') {
+            removeItem(e.target.parentNode, leftColumn);
+            addItem(e.target.parentNode, rightColumn);
+        }
+    });
+
+    rightColumn.addEventListener('click', function(e) {
+        if (e.target.id==='event_button') {
+            removeItem(e.target.parentNode, rightColumn);
+            addItem(e.target.parentNode, leftColumn);
+        }
+    });
+
+    document.addEventListener('mousedown', function(e) {
+        if (!e.target.classList.contains('wrap_item')) {
+            return null;
+        }
+        var item=e.target;
+        var move=moveItem.bind(null, item);
+
+        item.style.width=item.clientWidth+'px';
+        item.style.height=item.clientHeight+'px';
+        item.style.position='absolute';
+       
+        move(e);
+
+        document.body.appendChild(item);
+
+        item.style.zIndex=1000;
+
+        document.addEventListener('mousemove', move);
+
+        document.addEventListener('mouseup', function() {
+            document.removeEventListener('mousemove', move);
+        });
+    });
+}
+
 promise
     .then(function() {
         return api('friends.get', { count: 10, v: 5.68, fields: 'first_name, last_name, photo_100, city' });
@@ -36,6 +120,9 @@ promise
         var template = templateElement({ list: data.items });
 
         results.innerHTML = template;
+    })
+    .then(function() {
+        addListeners();
     })
     .catch(function(e) {
         alert(e.message);
